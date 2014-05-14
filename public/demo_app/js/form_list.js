9f52f074a10bf366694568a34e1cdd90095517eb
Ajay Modi <mitajakom@gmail.com>
2014-05-14 18:43:26 +0530

    partial progress form delete and response

---
+++ public/demo_app/js/form_list.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+ 
+    // delete the entry once we have confirmed that it should be deleted
+    $('#delete').click(function(e) {
+        var answer = confirm ("Are you sure you want to delete from the database?");
+        if (answer)
+        {
+
+
+          var parent = $(this).closest('tr');
+          $.ajax({
+              type: 'get',
+              url: 'delete.php', // <- replace this with your url here
+              data: 'ajax=1&delete=' + $(this).attr('id'),
+              beforeSend: function() {
+                  parent.animate({'backgroundColor':'#fb6c6c'},300);
+              },
+              success: function() {
+                  parent.fadeOut(300,function() {
+                      parent.remove();
+                  });
+              }
+          });
+        } else{
+          e.preventDefault();
+        }
+
+
+    });
+ 
+});
